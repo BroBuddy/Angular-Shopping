b505f5967442a4826d5e0f4a4e527879119963e6
BroBuddy <sirstone@gmx.de>
2018-10-11 00:00:00 +0200

    init shopping app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('Recipe Name 1', 'Recipe Description 1', 'https://via.placeholder.com/150x100'),
+    new Recipe('Recipe Name 2', 'Recipe Description 2', 'https://via.placeholder.com/150x100')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
