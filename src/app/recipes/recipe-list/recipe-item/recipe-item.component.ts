409901fcd0d1b23ed7927b67158b79ece77ef10b
Sebastian <matthias.sienz@emes-consulting.com>
2018-11-03 22:57:50 +0100

    clean up

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -7,11 +7,8 @@ import { Recipe } from '../../recipe.model';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
   @Input() recipe: Recipe;
   @Input() id: number;
-
-  ngOnInit() {
-  }
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+
 import { RecipeService } from '../recipes/recipe.service';
 import { AuthService } from '../auth/auth.service';
-
 import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
@@ -23,11 +23,24 @@ export class DataStorageService {
         const token = this.authService.getToken();
 
         this.http.get(this.apiUrl + 'recipes.json?auth=' + token)
-            .subscribe(
-                (response: Response) => {
-                    const recipes: Recipe[] = response.json();
-                    this.recipeService.setRecipes(recipes);
-                }
-            );
+        // .map(
+        //     (response: Response) => {
+        //         const recipes: Recipe[] = response.json();
+        //
+        //         for (const recipe of recipes) {
+        //             if (!recipe['ingredients']) {
+        //                 recipe['ingredients'] = [];
+        //             }
+        //         }
+        //
+        //         return recipes;
+        //     }
+        // )
+        .subscribe(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                this.recipeService.setRecipes(recipes);
+            }
+        );
     }
 }
